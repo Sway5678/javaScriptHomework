d354a994f0acf1bc0d6bea9dd908e4cccf0a7a8c
Sway5682 <gresnazar86@gmail.com>
2024-12-20 23:06:25 +0200

    add

---
+++ main.js
@@ -0,0 +1,104 @@
+/*
+send.addEventListener("click", function() {
+
+    let firstValue = document.getElementById("firstInput").value;
+    let secondValue = document.getElementById("secondInput").value;
+
+    if (firstValue === "" && secondValue === ""){
+
+        alert("Не всі поля заповнені")
+                                                                            
+ 
+    }
+
+    else {
+
+        alert("Обидва поля заповнені")
+
+    }
+})
+*/
+
+/*
+send.addEventListener("click", function() {
+
+    let firstValue = document.getElementById("firstInput").value;
+    let secondValue = document.getElementById("secondInput").value;
+
+    let sumaValue = firstValue.length + secondValue.length
+    if (sumaValue > 10){
+
+        alert("Сумма символів в полях вводу більше 10")
+                                                                            
+
+    }
+
+    else {
+
+        alert("Сумма символів в полях вводу менше, або дорівнює 10")
+
+    }
+
+})
+
+*/
+
+/*
+send.addEventListener("click", function() {
+
+    let firstValue = document.getElementById("firstInput").value;
+    let checkingFirstValue = firstValue.includes("JavaScript")
+
+    if (checkingFirstValue == true){
+
+        alert("Текст містить слово JavaScript")
+                                                                            
+    }
+
+    else {
+
+        alert("Текст не містить слово JavaScript")
+
+    }
+})
+
+send.addEventListener("click", function() {
+
+    let firstValue = Number(document.getElementById("firstInput").value);
+    let secondValue = Number(document.getElementById("secondInput").value);
+
+    let sumaValue = firstValue + secondValue
+
+    if (sumaValue >= 10 && sumaValue <= 20){
+
+        alert("Число входить в діапазон від 10 до 20")
+                                                                            
+    }
+
+    else {
+
+        alert("Число не входить в діапазон від 10 до 20")
+
+        }
+
+    })
+
+send.addEventListener("click", function () {
+
+  let firstValue = document.getElementById("firstInput").value.length;
+  let secondValue = document.getElementById("secondInput").value;
+  let thirdValue = document.getElementById("thirdInput").value.length;
+
+  if (firstValue < 3 || secondValue.includes("@") === false || secondValue.includes(".") === false || thirdValue < 6) {
+
+      alert(" Не вірно вказана інформація. Правильно заповнені дані: ім'я містить не менше 3 символів, email містить символ @ та крапку після неї, пароль містить не менше 6 символів. ")
+
+  }
+
+  else {
+
+      // тут мало бути переведення користувача на іншу сторінку, при правильному заповненні інпутів, але я не зрозумів як це зробити      
+
+        }
+})
+*/
